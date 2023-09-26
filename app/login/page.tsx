95c551db6b762bbd8d1783dcff71a24e8016a465
lamaison22 <joao_lamaison@hotmail.com>
2023-09-26 15:45:18 -0300

    base login type by user

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import Image from "next/image";
 import Form from "@/components/form";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+  const [opt, setOpt] = useState<'player' | 'gym'>('player')
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50 flex-col">
+      <div className="flex mb-4">
+        <button onClick={() => setOpt('player')} className={`mr-5 text-gray-400 hover:text-black ${opt === 'player' && 'text-black'}`}>Ginásio</button>
+        <button onClick={() => setOpt('gym')} className={`mr-5 text-gray-400 hover:text-black ${opt === 'gym' && 'text-black'}`}>Jogador</button>
+      </div>
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <Link href="/">
@@ -22,7 +31,11 @@ export default function Login() {
             Use your email and password to sign in
           </p>
         </div>
-        <Form type="login" />
+        {opt === 'player' ? (
+          <Form type="login" />
+        ) : (
+          <Form type="login" />
+        )}
       </div>
     </div>
   );
